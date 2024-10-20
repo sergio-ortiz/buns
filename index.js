8453665c3927d77bf56f557731285fbbd83d23dc
Sergio <ortiz.sergio0303@gmail.com>
2024-10-20 19:05:14 -0400

    feat(bun): in memory sqlite instead of array

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let todos = [];
+import { Database } from "bun:sqlite";
+const db = new Database(":memory:");
+const query = db.query("CREATE TABLE IF NOT EXISTS todos (todo TEXT NOT NULL)");
+query.run();
 
 Bun.serve({
 	async fetch(request) {
 		const url = new URL(request.url);
 
 		if(url.pathname === '/') {
-			todos = [];
 			return new Response(Bun.file("index.html"), {
 				headers: {
 					"Content-Type": "text/html",
@@ -16,9 +18,13 @@ Bun.serve({
 		if(url.pathname === '/create') {
 			const formdata = await request.formData();
 			const todo = formdata.get('todo');
-			todos.push(todo);
+			
+			const query2 = db.query(`INSERT INTO todos (todo) VALUES ("${todo}")`);
+			query2.run();
+			const query3 = db.query("SELECT * FROM todos");
+			const todos = query3.all();
 
-			const todolist = todos.map(todo => `<li>${todo}</li>`).join('');
+			const todolist = todos.map(({ todo }) => `<li>${todo}</li>`).join('');
 			return new Response(`<ul id="todos">${todolist}</ul>`, {
 				headers: {
 					"Content-Type": "text/html",
